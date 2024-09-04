6b417eb6d405aae2fcedc18981b8859ff17f9bfa
Marcin Holcman <62081203+HolTier@users.noreply.github.com>
2024-09-04 09:25:00 +0200

    Add GenericRepository and IGenericRepository for CRUD ops
    Introduce GenericRepository class and IGenericRepository interface
    in OurAM_Api.Data namespace. The GenericRepository class handles
    CRUD operations using Entity Framework Core, including methods for
    adding, removing, updating, and retrieving entities, as well as
    synchronous and asynchronous methods for saving changes. The
    IGenericRepository interface defines the contract for these
    operations and includes a method for retrieving an entity by name.

---
+++ OurAM-BACKEND/OurAM-Api/Data/GenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OurAM_Api.Data
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly OuramDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(OuramDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+            _context.SaveChanges();
+        }
+
+        public Task<T?> GetByNameAsync(string name)
+        {
+            return _dbSet.FirstOrDefaultAsync(e => e.GetType().GetProperty("Name").GetValue(e).ToString() == name);
+        }
+    }
+}
---
+++ OurAM-BACKEND/OurAM-Api/Data/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace OurAM_Api.Data
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<T?> GetByNameAsync(string name);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
